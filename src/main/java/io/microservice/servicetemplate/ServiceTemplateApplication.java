c533d28554267b7332f29b4efb1a556a294b03d5
ravuthz <ravuthz@gmail.com>
2020-12-26 01:44:47 +0700

    Register Template Service to Eureka Server

--- src/main/java/io/microservice/servicetemplate/ServiceTemplateApplication.java
+++ src/main/java/io/microservice/servicetemplate/ServiceTemplateApplication.java
@@ -2,12 +2,14 @@ package io.microservice.servicetemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceTemplateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceTemplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTemplateApplication.class, args);
+    }
 
 }
