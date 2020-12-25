ac845a10f109b4568abdc0eed0e053d5a539e4ab
ravuthz <ravuthz@gmail.com>
2020-12-26 00:57:12 +0700

    Add Spring Boot Web & Security Auto Configuration

---
+++ src/main/java/io/microservice/servicetemplate/controller/MainController.java
@@ -0,0 +1,18 @@
+package io.microservice.servicetemplate.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @Value("${spring.application.name}")
+    private String title;
+
+    @GetMapping
+    public String page() {
+        return "Welcome to " + title;
+    }
+
+}
